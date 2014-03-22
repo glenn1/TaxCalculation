2a832ec245a0c4f1cbc61dcbdb662558cfbc0380
Glenn Ostoforoff <eddcubby@gmail.com>
2014-03-22 08:53:12 -0700

    Create a console based application
    Create a console based application named TaxCalculation.Include two
    overloaded methods one that accepts a price  and a tax rate as doubles
    and one that accepts as integers.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double price = 79.95;
+            double dRate = 0.06;
+            int rate = 6;
+            calculateTax(price, rate);
+            calculateTax(price, dRate);
+            Console.ReadLine();
+        }
+        public static void calculateTax(double price, double rate)
+        {
+            double tax = price * rate;
+            Console.WriteLine("The tax is {0} with price of {1} and rate at {2}.",
+                tax.ToString("C"), price.ToString("C"), rate.ToString("C"));
+        }
+        public static void calculateTax(double price, int rate)
+        {
+            double newRate = rate * .01;
+            double tax = newRate * price;
+            Console.WriteLine("The tax is {0} with price of {1} and rate at {2}.",
+                tax.ToString("C"), price.ToString("C"), newRate.ToString("C"));
+
+        }
+    }
+}
